3b17f72a24e6ffb3d02031e0384a87a55027e052
tanphat132 <97350150+tanphat132@users.noreply.github.com>
2022-07-11 02:24:45 +0700

    Add files via upload

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kjei-z0zjJurwBr3I3Gc8nsIqv_zwXxOZdNe4iEFvb8/edit#gid=975033270';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
